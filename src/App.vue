ea9c1297e1cf3abbe514ea33b74eec06f720fd9e
Kona <kitami.kitano20@gmail.com>
2020-05-20 21:07:31 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <myheader />
+    <myheader />
+    <myheader />
+    <myheader />
+    <p v-if="msg.length > 0">{{ msg }}</p>
+    <p v-else>no text</p>
+    <input type="text" v-model="msg" />
+    <button @click="clear()">clear</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import myheader from "./components/myheader";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    myheader
+  },
+  data() {
+    return {
+      msg: "Hello World!"
+    };
+  },
+  methods: {
+    clear() {
+      this.msg = "";
+    }
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
