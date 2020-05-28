f16357edcc43c0027cab0f468df33087f6f94851
Kona <kitami.kitano20@gmail.com>
2020-05-28 12:45:26 +0900

    imageのresponsive対応

--- src/main/About.vue
+++ src/main/About.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="container is-fluid">
     <Title title="About" />
-    <div class="about container is-fluid">
+    <div class="about container">
       <p class="title has-text-white">kona</p>
       <br />
       <p>北海道に住んでいる大学4年です。</p>
       <i class="fas fa-address-book"></i>
       <p>工学部で情報を学んでいます。現在は主にWebの技術について学んでいます。</p>
-      <div class="columns is-desktop">
-        <div class="column">
+      <div class="columns is-mobile">
+        <div class="column is-one-third-desktop is-half-tablet is-full-mobile">
           <img class="image" src="../assets/img/kitami2.jpg" alt="sceen" />
         </div>
-        <div class="column">
+        <div class="column is-one-third-desktop is-half-tablet">
           <img class="image" src="../assets/img/winter-min.jpg" alt="sceen" />
         </div>
-        <div class="column">
+        <div class="column is-one-third-desktop">
           <img class="image saroma" src="../assets/img/Saroma-min.jpg" alt="sceen" />
         </div>
       </div>
@@ -37,12 +37,6 @@ export default {
 
 div {
   padding-bottom: $padding-bottom_pc;
-
-  .saroma {
-    @media (max-width: 600px) {
-      display: none;
-    }
-  }
 
   @media (max-width: 600px) {
     padding-bottom: $padding-bottom_mobile;
