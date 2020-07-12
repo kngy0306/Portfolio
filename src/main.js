a075aacf65f1e7ae115200882a49cba7c4890477
Kona <kitami.kitano20@gmail.com>
2020-07-12 18:57:35 +0900

    googleanalytics追加

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import Vue from "vue";
 import App from "./main/App.vue";
+
+import VueGtag from "vue-gtag";
 //fontawesome
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { fab } from "@fortawesome/free-brands-svg-icons"; // twitterのfaはbrandsだから
 
 library.add(fab);
+
+Vue.use(VueGtag, {
+  config: { id: "UA-172471134-1" },
+});
 
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 //fa end
